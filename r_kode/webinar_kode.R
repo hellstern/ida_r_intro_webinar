384d204d4bb5bccb4227a0de4157ea4221836247
Tue Hellstern (TUHE - Programleder - Cphbusiness) <tuhe@cphbusiness.dk>
2020-04-21 08:52:54 +0200

    new message

--- r_kode/webinar_kode.R
+++ r_kode/webinar_kode.R
@@ -177,15 +177,16 @@ ggplot(data=issalg) +
   geom_point(mapping = aes(x=Temperatur, y=Salg, color=Butik_Placering, size=Salg)) +
   geom_smooth(mapping = aes(x=Temperatur, y=Salg, color=Butik_Placering), se=FALSE) +
   geom_hline(yintercept = mean(issalg$Salg)) +
+  annotate("text", label="Middel Salg", x=15, vjust = 0, y=mean(issalg$Salg+200)) +
   geom_vline(xintercept = mean(issalg$Temperatur)) +
-  annotate("text", label="Middel temperatur", y=1, x=mean(issalg$Temperatur+4)) +
+  annotate("text", label="Middel Temperatur", y=100, hjust = 0, x=mean(issalg$Temperatur-0.5), angle = 90) +
   theme(panel.background = element_blank()) +
   theme(plot.background = element_blank()) +
   scale_x_continuous(name="Temperatur") +
   scale_y_continuous(name="Total Salg") +
   scale_color_discrete(name="Placering af butiken") +
-  scale_size_continuous(name="Salg")
-
+  scale_size_continuous(name="Salg") +
+  theme_classic()
 
 # ******************************
 # Plot ggplo2 - Salg - Group By
